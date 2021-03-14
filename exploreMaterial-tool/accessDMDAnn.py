c4462bd8459be508650e0520f216c6382f3df6d3
pncanas <pncanas@vicomtech.org>
2021-03-14 01:24:52 +0100

    add depth compatibility

--- exploreMaterial-tool/accessDMDAnn.py
+++ exploreMaterial-tool/accessDMDAnn.py
@@ -11,6 +11,7 @@ import time
 from vcd4reader import VcdHandler
 from vcd4reader import VcdDMDHandler
 
+import ffmpeg
 # Written by Paola Cañas with <3
 
 # Python and Opencv script to prepare/export material of DMD for training.
@@ -21,7 +22,7 @@ from vcd4reader import VcdDMDHandler
 # -----
 class exportClass():
 
-    def __init__(self, vcdFile, rootDmd, destinationPath, datasetDMD=True): 
+    def __init__(self, vcdFile, rootDmd, destinationPath, datasetDMD=True):
         # ------ GLOBAL VARIABLES ------
         # - Args -
         self.vcdFile = vcdFile
@@ -35,9 +36,9 @@ class exportClass():
             self.vcd_handler = VcdDMDHandler(vcd_file=Path(self.vcdFile))
             # @self.info: [group, subject, session, date]
             self.info = self.vcd_handler.get_basic_metadata()
-            # Just get day and hour from the full timestamp        
-            self.dateDayHour = self.info[3]        
-            # @shifts: [body_face_sh, hands_face_sh, hands_body_sh]        
+            # Just get day and hour from the full timestamp
+            self.dateDayHour = self.info[3]
+            # @shifts: [body_face_sh, hands_face_sh, hands_body_sh]
             self.shift_bf, self.shift_hf, self.shift_hb = self.vcd_handler.get_shifts()
         else:
             # Create VCD Reader object
@@ -50,6 +51,7 @@ class exportClass():
 
         # @self.actionList: ["driver_actions/safe_drive", "gaze_on_road/looking_road",.. , ..]
         self.actionList = self.vcd_handler.get_action_type_list()
+        self.frameNum = 0
 
 
         # -- CONTROL VARIABLES --
@@ -62,6 +64,9 @@ class exportClass():
 
         @streams: list of camera names to export material from.
         Possible values: "face", "body", "hands", "general"
+
+        @channels: list of channels of information you wish to export .
+        Possible values: "rgb", "ir", "depth"
         
         @annotations: list of classes you wish to export (e.g. ["safe_drive","drinking"])
         Possible values: all labels names
@@ -86,21 +91,21 @@ class exportClass():
         # config
         material = ["image"]
         streams = ["body","face", "hands"] #must be "general" if not DMD dataset
-        channels = ["rgb", "ir"] #must be only "rgb" if not DMD dataset
+        channels = ["rgb", "ir"] #Include "depth" to export Depth information too. It must be only "rgb" if not DMD dataset
         annotations = self.actionList
         write = True
         intervalChunk = 0
-        
+
         #validations
         if not self.datasetDMD and (streams[0] != "general" or len(streams)> 1):
             raise RuntimeError(
-                                "WARNING: stream option for other datasets must be only 'general'")
+                "WARNING: stream option for other datasets must be only 'general'")
         if not self.datasetDMD and len(channels)> 1:
             raise RuntimeError(
-                                "WARNING: channles option for other datasets must be only 'rgb'")
+                "WARNING: channles option for other datasets must be only 'rgb'")
         #exec
         self.exportMaterial(material, streams, channels, annotations, write, intervalChunk, True)
-        
+
     # @material: list of data format to export (e.g. ["image","video"])
     # @streams: list of camera names to export (e.g. ["body", "hands"])
     # @annotations: list of annotations to export (e.g. ["driver_actions/reach_side", "talking/talking","hands_using_wheel/only_left"])
@@ -140,9 +145,10 @@ class exportClass():
             annotation = self.actionList[annotation]
 
         print("\n\n-- Creating %s of action: %s --" % (material, str(annotation)))
-        
+
         # Check and load valid video
-        capVideo = cv2.VideoCapture(str(self.getStreamVideo(channel, stream)))
+        streamVideoPath = str(self.getStreamVideo(channel, stream))
+        capVideo = cv2.VideoCapture(streamVideoPath)
         # get intervals from vcd
         fullIntervals = self.vcd_handler.get_frames_intervals_of_action(annotation)
         # make lists from dictionaries
@@ -165,20 +171,33 @@ class exportClass():
             if not dirName.exists():
                 os.makedirs(str(dirName), exist_ok=True)
                 print("Directory", dirName.name, "created")
+            
+            #@depthVideoArray: numpy array with all frames of video containing depth information
+            depthVideoArray = []
+            if channel == "depth" and (material == "image" or material == "images" ):
+                depthVideoArray = self.getDepthVideoArray(streamVideoPath)
 
             for count, interval in enumerate(fullIntervalsAsList):
-                
+
                 # Check if frames are avalabile in stream. Find corresponding frames in stream, rigth now is mosaic frame
                 valid, startFrame, endFrame = self.checkFrameInStream(
                     stream, interval[0], interval[1])
 
                 if valid:
                     print('Exporting interval %d \r' % count, end="")
-                    fileName = str(dirName) + "/" + stream + "_" + self.dateDayHour + "_"+self.info[1]+ "_"+str(count)  # Name with stream, date, subject and interval id to not overwrite
+                    # Name with stream, date, subject and interval id to not overwrite
+                    fileName = str(dirName) + "/" + stream + "_" + \
+                        self.dateDayHour + "_"+self.info[1] + "_"+str(count)
                     if material == "image" or material == "images" or material == "img" or material == "imgs":
-                        self.frameIntervalToImages(startFrame, endFrame, capVideo, fileName)
+                        if channel == "depth":
+                            self.depthFrameIntervalToImages(startFrame, endFrame, depthVideoArray, fileName)
+                        else:
+                            self.frameIntervalToImages(startFrame, endFrame, capVideo, fileName)
                     else:
-                        self.frameIntervalToVideo(startFrame, endFrame, capVideo, fileName)
+                        if channel == "depth":
+                            self.depthFrameIntervalToVideo(startFrame, endFrame, streamVideoPath, fileName)
+                        else:
+                            self.frameIntervalToVideo(startFrame, endFrame, capVideo, fileName)
                 else:
                     print(
                         "WARNING: Skipped interval %i, because some of its frames do not exist in stream %s" %(count,stream))
@@ -209,16 +228,20 @@ class exportClass():
 
         for interval in intervals:
             framesLost = 0
-            dist = interval[1] - interval[0]
+            init = interval[0]
+            end = interval[1]
+            if end == self.frameNum-1:
+                end = end-1
+            dist = end - init
             framesSum = framesSum + dist
-            count = interval[0] if asc else interval[1]
+            count = init if asc else end
 
             # calculate how many chunks will result per interval
             numOfChunks = math.floor(dist / intervalChunk)
             # If the division of interval is not possible and cannot be ignored
             if numOfChunks <= 0 and not ignoreSmall:
                 raise RuntimeError("WARNING: the interval chunk length chosen is too small, some intervals are too small to be cutted by",
-                                intervalChunk, "frames. To ignore small intervals, set True to ignoreSmall argument.")
+                                   intervalChunk, "frames. To ignore small intervals, set True to ignoreSmall argument.")
             else:
                 # if the division of interval is possible
                 if numOfChunks > 0:
@@ -232,14 +255,14 @@ class exportClass():
                             intervalsCutted.append([count, count - intervalChunk])
                             count = count - intervalChunk
                     framesLost = abs(
-                        count - interval[1]) if asc else abs(count - interval[0])
+                        count - end) if asc else abs(count - init)
                 else:
                     print("WARNING: Skipped interval",
-                        interval, "for being too small :(")
+                          interval, "for being too small :(")
             framesLostSum = framesLostSum + framesLost
         print("Total frame loss:", framesLostSum, "of total:", framesSum)
         print("Resulting number of intervals:", len(intervalsCutted),
-            "from initial number:", len(intervals))
+              "from initial number:", len(intervals))
 
         return intervalsCutted
 
@@ -259,6 +282,22 @@ class exportClass():
             intervalVideo.write(image)
         intervalVideo.release()
 
+    # Function to create a sub video called @name.avi from @frameStart to @frameEnd of stream video @streamVideoPath from DEPTH channel.
+    # Uses ffmpeg-python to properly cut the video
+    # Cut must be made with time, not with frame number. That is why it is frameNumber/frameRate
+    # saves video in @self.destinationPath
+    # @streamVideoPath: is the depth video path, not video
+    # @name of file with no extension
+    def depthFrameIntervalToVideo(self, frameStart, frameEnd, streamVideoPath, name):
+
+        vid = (
+            ffmpeg.input(streamVideoPath)
+            .trim(start=int(frameStart/29.76), end=int(frameEnd/29.76))
+            .setpts('PTS-STARTPTS')
+            .output(name+".avi", vcodec='ffv1', pix_fmt='gray16le', crf=0)
+            .run()
+        )
+
     # Function to get images from @frameStart to @frameEnd of stream video @capVideo
     # saves in @self.destinationPath
     # @capVideo: is video loaded in opencv, not path
@@ -272,6 +311,33 @@ class exportClass():
             cv2.imwrite(name+"_"+str(count)+".jpg", image)
             count += 1
 
+    # Function to get images from @frameStart to @frameEnd of stream DEPTH info array @depthVideoArray
+
+    # saves in @self.destinationPath
+    # @name of images with no extension
+    def depthFrameIntervalToImages(self, frameStart, frameEnd, depthVideoArray, name):
+        for i in range(frameStart,frameEnd+1):
+            if i != self.frameNum:
+                cv2.imwrite(name+"_"+str(i)+".png",depthVideoArray[i])
+            else:
+                #write a black image
+                cv2.imwrite(name+"_"+str(i)+".png",np.zeros((720,1280),dtype=np.uint16))
+
+    # Function to get depth information of all frames from depth video in a unit16 array. Array should be [self.frameNum,720,1280]
+    # Uses ffmpeg-python to properly extract info from video in gray16le pixelformat
+    def getDepthVideoArray(self, streamVideoPath):
+        out, _ = (
+            ffmpeg
+            .input(streamVideoPath)
+            .output('pipe:', format='rawvideo', pix_fmt='gray16le')
+            .run(capture_stdout=True)
+        )
+        array_imgs = (
+            np
+            .frombuffer(out, np.uint16)
+            .reshape([-1, 720, 1280])
+        )
+        return array_imgs
 
     # Function to get uri of the @videoStream video from VCD and check if video frame count matches with VCD.
     # Returns @videoPath: path of @videoStream in VCD
@@ -281,21 +347,25 @@ class exportClass():
         if self.datasetDMD:
             if videoStream == "face":
                 videoPath = self.vcd_handler.get_videos_uris()[0]
-                frameNum = self.vcd_handler.get_frame_numbers()[0]
+                self.frameNum = self.vcd_handler.get_frame_numbers()[0]
             elif videoStream == "body":
                 videoPath = self.vcd_handler.get_videos_uris()[1]
-                frameNum = self.vcd_handler.get_frame_numbers()[1]
+                self.frameNum = self.vcd_handler.get_frame_numbers()[1]
             elif videoStream == "hands":
                 videoPath = self.vcd_handler.get_videos_uris()[2]
-                frameNum = self.vcd_handler.get_frame_numbers()[2]
+                self.frameNum = self.vcd_handler.get_frame_numbers()[2]
             else:
-                raise RuntimeWarning(videoStream, ": Not a valid video stream. Must be: 'face', 'body' or 'hands'.")
-            #change to desire channel video path
-            videoPath = videoPath.replace("rgb",videoChannel)
+                raise RuntimeWarning(
+                    videoStream, ": Not a valid video stream. Must be: 'face', 'body' or 'hands'.")
+            # change to desire channel video path
+            videoPath = videoPath.replace("rgb", videoChannel)
+            if videoChannel == "depth":
+                videoPath = videoPath.replace("mp4", "avi")
+
         else:
             if videoStream =="general":
                 videoPath = self.vcd_handler.get_videos_uri()
-                frameNum = self.vcd_handler.get_frames_number()
+                self.frameNum = self.vcd_handler.get_frames_number()
             else:
                 raise RuntimeWarning(videoStream,": Not a valid video stream. Must be 'general'")
 
@@ -307,15 +377,19 @@ class exportClass():
         if videoPath.exists():
             cap = cv2.VideoCapture(str(videoPath))
             length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            if length != frameNum:
-                raise RuntimeWarning(
-                    "VCD's and real video frame count don't match.")
+            if length != self.frameNum:
+                #Some depth videos are missing 1 frame
+                if videoChannel == "depth":
+                    self.frameNum = self.frameNum - 1
+                else:
+                    raise RuntimeWarning(
+                        "VCD's and real video frame count don't match.")
             else:
                 print(videoChannel, videoStream, "stream loaded:", videoPath.name)
         else:
             raise RuntimeError(
                 videoPath, "video not found. Video Uri in VCD is wrong or video does not exist")
-        
+
         return videoPath
 
     #Function to check if the mosaic-count frame is available in stream requested. Then calculate corresponding frame position in stream-count
@@ -331,7 +405,7 @@ class exportClass():
             # Face starts first
             if stream == "face":
                 if frameEnd < faceLen:
-                    return True, frameStart, frameEnd 
+                    return True, frameStart, frameEnd
             elif stream == "body":
                 if frameStart - self.shift_bf >= 0 and frameEnd - self.shift_bf <= bodyLen:
                     return True, frameStart - self.shift_bf, frameEnd - self.shift_bf
@@ -345,7 +419,7 @@ class exportClass():
 
             if stream == "body":
                 if frameEnd < bodyLen:
-                    return True, frameStart, frameEnd 
+                    return True, frameStart, frameEnd
             elif stream == "face":
                 if frameStart + self.shift_bf >= 0 and frameEnd + self.shift_bf <= faceLen:
                     return True, frameStart + self.shift_bf, frameEnd + self.shift_bf
@@ -358,7 +432,7 @@ class exportClass():
             #shifts = [-self.shift_hf, -self.shift_hb]
             if stream == "hands":
                 if frameEnd < handsLen:
-                    return True, frameStart, frameEnd 
+                    return True, frameStart, frameEnd
             elif stream == "body":
                 if frameStart + self.shift_hb >= 0 and frameEnd + self.shift_hb <= bodyLen:
                     return True, frameStart + self.shift_hb, frameEnd + self.shift_hb
@@ -384,5 +458,5 @@ class exportClass():
         return {int(k) if self.is_string_int(k) else k: v for k, v in x}
 
 
-    
+
 
