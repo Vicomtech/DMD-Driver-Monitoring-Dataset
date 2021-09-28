9b67802908b50f2f0b8f1a88f839d1c9c166c2a6
pncanas <pncanas@vicomtech.org>
2021-09-28 13:34:40 +0200

    add statistics functionality

--- exploreMaterial-tool/DExTool.py
+++ exploreMaterial-tool/DExTool.py
@@ -6,9 +6,10 @@ import re
 from pathlib import Path
 from accessDMDAnn import exportClass
 from group_split_material import splitClass, groupClass
+from statistics import get_statistics
 
 print("Welcome :)")
-opt = int(input("What do you whish to do?:  export material for training:[0]  group exported material by classes:[1]  create train and test split:[2] : "))
+opt = int(input("What do you whish to do?:  export material for training:[0]  group exported material by classes:[1]  create train and test split:[2]  get statistics:[3] : "))
 
 if opt == 0:
     # export material for training
@@ -126,5 +127,105 @@ elif opt == 2:
     splitClass(material_path,destination_path,test_proportion)
 
     print("Oki :) ----------------------------------------")
+
+elif opt == 3:
+    # Get statistics
+    print("This function only works with dmd material structure when exporting with DEx tool.")
+    destination_path = input("Enter destination filename: ")
+    selec = input("How do you want to read annotations, by: Group:[g]  Sessions:[f]  One VCD:[v] : ")
+    
+    #Delete destination_path to avoid redundancies
+    if os.path.exists(destination_path.replace(".txt","actions.txt")):
+        os.remove(destination_path.replace(".txt","actions.txt"))
+    if os.path.exists(destination_path.replace(".txt","frames.txt")):
+        os.remove(destination_path.replace(".txt","frames.txt"))
+
+    if selec == "g":
+        #By group
+        folder_path = input("Enter DMD group's path (../dmd/g#): ")
+        #e.g /home/pncanas/Desktop/consumption/dmd/gA
+        selec_session = input("Enter the session you wish to export in this group:  all:[0]  S1:[1]  S2:[2]  S3[3]  S4[4] : ")
+
+        subject_paths = glob.glob(folder_path + '/*')
+        subject_paths.sort()
+
+        for subject in subject_paths:
+            print(subject)
+            session_path = glob.glob(subject + '/*')
+            session_path.sort()
+
+            for session in session_path:
+                if "s"+str(selec_session) in session or selec_session == "0":
+                    print(session)
+                    annotation_paths = glob.glob(session + '/*.json')
+                    annotation_paths.sort()
+                    for annotation in annotation_paths:
+                        print(annotation)
+                        
+                        get_statistics(annotation,destination_path)
+
+                        print("Oki :) ----------------------------------------")
+
+    elif selec == "f":
+        #By session
+        folder_path = input("Enter root dmd folder path(../dmd): ")
+        #e.g /home/pncanas/Desktop/dmd/
+        selec_session = input("Enter the session you wish to export in this group:  all:[0]  S1:[1]  S2:[2]  S3[3]  S4[4] : ")
+
+        group_paths = glob.glob(folder_path + '/*')
+        group_paths.sort()
+
+        for group in group_paths:
+            print(group)
+            subject_paths = glob.glob(group + '/*')
+            subject_paths.sort()
+
+            for subject in subject_paths:
+                print(subject)
+                session_path = glob.glob(subject + '/*')
+                session_path.sort()
+
+                for session in session_path:
+                    if "s"+str(selec_session) in session or selec_session == "0":
+                        print(session)
+                        annotation_paths = glob.glob(session + '/*.json')
+                        annotation_paths.sort()
+
+                        for annotation in annotation_paths:
+                            print(annotation)
+                            dmd_folder=Path(annotation).parents[3]
+
+                            get_statistics(annotation,destination_path)
+                            
+                            print("Oki :) ----------------------------------------")
+
+    elif selec == "v":
+
+        vcd_path = input("Paste the vcd file path (..._ann.json): ")
+        # e.g: /Desktop/consumption/dmd/gA/1/s2/gA_1_s2_2019-03-08T09;21;03+01;00_rgb_ann.json
+        regex_internal = '(?P<subject>[1-9]|[1-2][0-9]|[3][0-7])_(?P<session>[a-z]{1,})_'\
+                '(?P<date>(?P<month>0[1-9]|1[012])-(?P<day>0[1-9]|[12][0-9]|3[01]))'
+        regex_external = '(?P<group>g[A-z]{1,})_(?P<subject>[1-9]|[1-2][0-9]|[3][0-7])_'\
+            '(?P<session>s[1-9]{1,})_(?P<timestamp>(?P<date>(?P<year>\d{4})-(?P<month>0[1-9]|1[012])-'\
+                '(?P<day>0[1-9]|[12][0-9]|3[01]))T(?P<time>(?P<hour>\d{1,2});(?P<minute>\d{1,2});'\
+                    '(?P<second>\d{1,2}))\+\d{1,2};\d{1,2})_(?P<channel>rgb|depth|ir)_(?P<stream>ann)'
+        regex_internal = re.compile(regex_internal)
+        regex_external = re.compile(regex_external)
+        match_internal = regex_internal.search(str(vcd_path))
+        match_external = regex_external.search(str(vcd_path))
+
+        if match_internal or match_external:
+            #dmd annotation
+            dmd_folder=Path(vcd_path).parents[3]
+        else:
+            #not a dmd annotation
+            dmd_folder=Path(vcd_path).parents[1]
+
+        get_statistics(vcd_path,destination_path)
+
+        print("Oki :) ----------------------------------------")       
+
+    else:
+        print("__Please, select a valid option__")
 else:
     print("__Please, put a valid option.__")
---
+++ exploreMaterial-tool/statistics.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import cv2
+from pathlib import Path  # To handle paths independent of OS
+import numpy as np
+from numpy.lib.function_base import append
+from vcd.utils import filter_outside
+
+# Import local class to parse VCD content
+from vcd4reader import VcdHandler
+from vcd4reader import VcdDMDHandler
+
+class get_statistics():
+
+    def __init__(self, vcdFile, destinationFile):
+
+        self.vcdFile = vcdFile
+        self.vcd_handler = VcdHandler(vcd_file=Path(self.vcdFile))
+
+        self.actionPath = destinationFile.replace(".txt","actions.txt")
+        self.framesPath = destinationFile.replace(".txt","frames.txt")
+
+        # @self.actionList: ["driver_actions/safe_drive", "gaze_on_road/looking_road",.. , ..]
+        self.actionList = self.vcd_handler.get_action_type_list()
+        #Get object list
+        self.objectList = self.vcd_handler.get_object_type_list()
+        #from 1 to not get "driver" object
+        for object in self.objectList: 
+            # Append objects to actionList
+            if "driver" in object:
+                #Dont add driver object
+                continue
+            self.actionList.append("objects_in_scene/"+object)
+        
+        self.countActions()
+        self.countFrames()
+
+    def countActions(self):
+        string_txt = []
+
+        if os.path.exists(self.actionPath):
+            with open(self.actionPath, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    string_txt.append(line.split(":"))
+
+            #Delete to avoid redundancy
+            os.remove(self.actionPath)
+
+        for annotation in self.actionList:
+            sum = 0
+            # Check if annotation is an object or an action
+            if "object" in annotation:
+                # get object intervals from vcd
+                fullIntervals = self.vcd_handler.get_frames_intervals_of_object(annotation)
+            else:
+                # get action intervals from vcd
+                fullIntervals = self.vcd_handler.get_frames_intervals_of_action(annotation)
+
+            #sum all frames in intervals
+            for interval in fullIntervals:
+                sum = sum + int(interval["frame_end"]) - int(interval["frame_start"])
+
+            found = False
+            #replace the sum for the new one if annotation found in txt
+            for num, line in enumerate(string_txt):
+                if annotation in line[0]:
+                    found = True
+                    string_txt[num][1] = str(sum+int(string_txt[num][1]))+"\n"
+
+            #if not found, add the sum
+            if not found:
+                string_txt.append([annotation,str(sum)+"\n"])
+
+        #write
+        file = open(self.actionPath, "a+")
+        for line in string_txt:
+            print("write",line)
+            file.write(line[0]+":"+line[1])
+        file.close()
+
+    def countFrames(self):
+            sum = 0
+            if os.path.exists(self.framesPath):
+                with open(self.framesPath, "r") as f:
+                    lines = f.read()
+                    sum = int(lines.split(":")[1])
+                        
+                #Delete to avoid redundancy
+                os.remove(self.framesPath)
+
+            sum = sum + self.vcd_handler.get_frames_number()
+
+            #write
+            file = open(self.framesPath, "a+")
+            file.write("total_frames"+":"+str(sum))
+            file.close()
